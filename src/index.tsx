a88bc7f70ea981b076a720f7f36a141e2afc23ef
Thanh Son Dang <sondang3298_hp@yahoo.com.vn>
2022-01-04 19:44:30 +0100

    localhost config for usedapp

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,25 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
+import { DAppProvider, Config, Localhost } from '@usedapp/core'
 import App from './App';
 import theme from './theme';
 
+const config: Config = {
+  readOnlyChainId: 1337,
+  networks: [Localhost],
+  multicallAddresses: {
+    //1337: "Your_Own_Multicall_Address",
+  }
+}
+
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    <CssBaseline />
-    <App />
+    <DAppProvider config={config}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </DAppProvider>
   </ThemeProvider>,
   document.querySelector('#root'),
 );
